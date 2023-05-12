b878c33df098aba12c2774e4b80157d81f3c8d8c
SmileSmith <longde_chen@163.com>
2023-05-12 20:59:44 +0800

    feat: 一些个人信息更换

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -1,4 +1,5 @@
 import { ss } from '@/utils/storage'
+import defaultAvatar from '@/assets/avatar.jpg'
 
 const LOCAL_NAME = 'userStorage'
 
@@ -15,9 +16,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: defaultAvatar,
+      name: '龙的传人',
+      description: 'Star on <a href="https://github.com/SmileSmith/chatgpt-proxy" class="text-blue-500" target="_blank" >GitHub</a>',
     },
   }
 }
